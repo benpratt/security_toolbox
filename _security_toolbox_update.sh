5894d5946aac9d89323251842fae1028bc1efcb5
Ben Pratt <ben@thegeekzone.com>
2012-09-02 21:12:15 -0500

    Added some additional output when script is running.

--- _security_toolbox_update.sh
+++ _security_toolbox_update.sh
@@ -8,11 +8,13 @@ then
 echo "Updating Metasploit from github"
 cd metasploit_git
 git pull
+echo "Metasploit from github updated"
 echo ""
 cd ..
 else
 echo "Getting Metasploit from github"
 git clone https://github.com/rapid7/metasploit-framework.git metasploit_git
+echo "Metasploit from github downloaded"
 echo ""
 fi
 
@@ -22,11 +24,13 @@ then
 echo "Updating Metasploit from svn"
 cd metasploit_svn
 svn update
+echo "Metasploit from svn updated"
 echo ""
 cd ..
 else
 echo "Getting Metasploit from svn"
 svn co https://www.metasploit.com/svn/framework3/trunk/ metasploit/
+echo "Metasploit from svn downloaded"
 echo ""
 fi
 
@@ -36,11 +40,13 @@ then
 echo "Updating Nmap"
 cd nmap
 svn update
+echo "Nmap updated"
 echo ""
 cd ..
 else
 echo "Getting Nmap"
 svn co --username guest --password "" https://svn.nmap.org/nmap/ nmap/
+echo "Nmap downloaded"
 echo ""
 fi
 
@@ -50,11 +56,13 @@ then
 echo "Updating PuTTY"
 cd putty
 svn update
+echo "PuTTY updated"
 echo ""
 cd ..
 else
 echo "Getting PuTTY"
 svn co svn://svn.tartarus.org/sgt/putty putty/
+echo "PuTTY downloaded"
 echo ""
 fi
 
@@ -64,11 +72,13 @@ then
 echo "Updating Reconnoiter"
 cd reconnoiter
 svn update
+echo "Reconnoiter updated"
 echo ""
 cd ..
 else
 echo "Getting Reconnoiter"
 svn co https://reconnoiter.svn.sourceforge.net/svnroot/reconnoiter reconnoiter/
+echo "Reconnoiter downloaded"
 echo ""
 fi
 
@@ -78,11 +88,13 @@ then
 echo "Updating Samurai"
 cd samurai
 svn update
+echo "Samurai updated"
 echo ""
 cd ..
 else
 echo "Getting Samurai"
 svn co https://samurai.svn.sourceforge.net/svnroot/samurai samurai/
+echo "Samurai downloaded"
 echo ""
 fi
 
@@ -92,11 +104,13 @@ then
 echo "Updating SET"
 cd set
 svn update
+echo "SET updated"
 echo ""
 cd ..
 else
 echo "Getting SET"
 svn co http://svn.secmaniac.com/social_engineering_toolkit set/
+echo "SET downloaded"
 echo ""
 fi
 
@@ -106,11 +120,13 @@ then
 echo "Updating sqlmap"
 cd sqlmap
 git pull
+echo "sqlmap updated"
 echo ""
 cd ..
 else
 echo "Getting sqlmap"
 git clone https://github.com/sqlmapproject/sqlmap.git sqlmap
+echo "sqlmap downloaded"
 echo ""
 fi
 
@@ -120,11 +136,13 @@ then
 echo "Updating w3af"
 cd w3af
 svn update
+echo "w3af updated"
 echo ""
 cd ..
 else
 echo "Getting w3af"
 svn co https://w3af.svn.sourceforge.net/svnroot/w3af/trunk w3af/
+echo "w3af downloaded"
 echo ""
 fi
 
@@ -134,11 +152,13 @@ then
 echo "Updating waffit"
 cd waffit
 svn update
+echo "waffit updated"
 echo ""
 cd ..
 else
 echo "Getting waffit"
 svn co http://waffit.googlecode.com/svn/trunk/ waffit/
+echo "waffit downloaded"
 echo ""
 fi
 
@@ -148,11 +168,13 @@ then
 echo "Updating dnsrecon from github"
 cd dnsrecon
 git pull
+echo "dnsrecon updated"
 echo ""
 cd ..
 else
 echo "Getting dnsrecon from github"
 git clone https://github.com/darkoperator/dnsrecon.git dnsrecon
+echo "dnsrecon downloaded"
 echo ""
 fi
 
@@ -162,11 +184,13 @@ then
 echo "Updating hash-identifier"
 cd hash-identifier
 svn update
+echo "hash-identifier updated"
 echo ""
 cd ..
 else
 echo "Getting hash-identifier"
 svn checkout http://hash-identifier.googlecode.com/svn/trunk/ hash-identifier
+echo "hash-identifier downloaded"
 echo ""
 fi
 
@@ -176,11 +200,13 @@ then
 echo "Updating theharvester"
 cd theharvester
 svn update
+echo "theharvester updated"
 echo ""
 cd ..
 else
 echo "Getting theharvester"
 svn checkout http://theharvester.googlecode.com/svn/trunk/ theharvester
+echo "theharvester downloaded"
 echo ""
 fi
 
@@ -190,11 +216,13 @@ then
 echo "Updating Nmap-Tools by SpiderLabs from github"
 cd nmap-tools
 git pull
+echo "Nmap-Tools updated"
 echo ""
 cd ..
 else
 echo "Getting Nmap-Tools by SpiderLabs from github"
 git clone https://github.com/SpiderLabs/Nmap-Tools.git nmap-tools
+echo "Nmap-Tools downloaded"
 echo ""
 fi
 
