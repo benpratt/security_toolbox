0bfbddf6a530a6d98735bea8e477e86e115440f5
Ben <ben@thegeekzone.com>
2017-09-26 15:21:57 -0500

    Update _security_toolbox_update.sh
    Added a check so not all code needs to be downloaded

--- _security_toolbox_update.sh
+++ _security_toolbox_update.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Metasploit from github
+if grep -q "*metasploit_git" ./.security_toolbox
+then
 if test -d metasploit_git
 then
 echo "Updating Metasploit from github"
@@ -15,8 +17,11 @@ git clone https://github.com/rapid7/metasploit-framework.git metasploit_git
 echo "Metasploit from github downloaded"
 echo ""
 fi
+fi
 
 # Nmap
+if grep -q "*nmap" ./.security_toolbox
+then
 if test -d nmap
 then
 echo "Updating Nmap"
@@ -31,8 +36,11 @@ svn co --username guest --password "" https://svn.nmap.org/nmap/ nmap/
 echo "Nmap downloaded"
 echo ""
 fi
+fi
 
 # PuTTY
+if grep -q "*putty" ./.security_toolbox
+then
 if test -d putty
 then
 echo "Updating PuTTY"
@@ -47,8 +55,11 @@ git clone https://git.tartarus.org/simon/putty.git putty
 echo "PuTTY downloaded"
 echo ""
 fi
+fi
 
 # Reconnoiter
+if grep -q "*reconnoiter" ./.security_toolbox
+then
 if test -d reconnoiter
 then
 echo "Updating Reconnoiter"
@@ -63,8 +74,11 @@ svn co https://svn.code.sf.net/p/reconnoiter/code/ reconnoiter/
 echo "Reconnoiter downloaded"
 echo ""
 fi
+fi
 
 # Samurai
+if grep -q "*samurai" ./.security_toolbox
+then
 if test -d samurai
 then
 echo "Updating Samurai"
@@ -79,8 +93,11 @@ svn co https://svn.code.sf.net/p/samurai/code/ samurai/
 echo "Samurai downloaded"
 echo ""
 fi
+fi
 
 # SET
+if grep -q "*set" ./.security_toolbox
+then
 if test -d set
 then
 echo "Updating SET"
@@ -95,8 +112,11 @@ git clone https://github.com/trustedsec/social-engineer-toolkit/ set/
 echo "SET downloaded"
 echo ""
 fi
+fi
 
 # sqlmap
+if grep -q "*sqlmap" ./.security_toolbox
+then
 if test -d sqlmap
 then
 echo "Updating sqlmap"
@@ -111,8 +131,11 @@ git clone https://github.com/sqlmapproject/sqlmap.git sqlmap
 echo "sqlmap downloaded"
 echo ""
 fi
+fi
 
 # w3af
+if grep -q "*w3af" ./.security_toolbox
+then
 if test -d w3af
 then
 echo "Updating w3af"
@@ -126,6 +149,7 @@ echo "Getting w3af"
 git clone https://github.com/andresriancho/w3af.git w3af
 echo "w3af downloaded"
 echo ""
+fi
 fi
 
 # waffit
@@ -145,6 +169,8 @@ fi
 #fi
 
 # wafw00f from github
+if grep -q "*wafw00f" ./.security_toolbox
+then
 if test -d wafw00f
 then
 echo "Updating wafw00f from github"
@@ -159,8 +185,11 @@ git clone https://github.com/EnableSecurity/wafw00f.git wafw00f
 echo "wafw00f downloaded"
 echo ""
 fi
+fi
 
 # dnsrecon from github
+if grep -q "*dnsrecon" ./.security_toolbox
+then
 if test -d dnsrecon
 then
 echo "Updating dnsrecon from github"
@@ -174,6 +203,7 @@ echo "Getting dnsrecon from github"
 git clone https://github.com/darkoperator/dnsrecon.git dnsrecon
 echo "dnsrecon downloaded"
 echo ""
+fi
 fi
 
 # hash-identifier
@@ -193,6 +223,8 @@ fi
 #fi
 
 # hashcat from github
+if grep -q "*hashcat" ./.security_toolbox
+then
 if test -d hashcat
 then
 echo "Updating hashcat from github"
@@ -207,8 +239,11 @@ git clone https://github.com/hashcat/hashcat.git hashcat
 echo "hashcat downloaded"
 echo ""
 fi
+fi
 
 # theHarvester from github
+if grep -q "*theHarvester" ./.security_toolbox
+then
 if test -d theHarvester
 then
 echo "Updating theHarvester from github"
@@ -223,8 +258,11 @@ git clone https://github.com/laramies/theHarvester.git theHarvester
 echo "theHarvester downloaded"
 echo ""
 fi
+fi
 
 # Nmap-Tools by SpiderLabs from github
+if grep -q "*nmap-tools" ./.security_toolbox
+then
 if test -d nmap-tools
 then
 echo "Updating Nmap-Tools by SpiderLabs from github"
@@ -239,8 +277,11 @@ git clone https://github.com/SpiderLabs/Nmap-Tools.git nmap-tools
 echo "Nmap-Tools downloaded"
 echo ""
 fi
+fi
 
 # htty from github
+if grep -q "*htty" ./.security_toolbox
+then
 if test -d htty
 then
 echo "Updating htty from github"
@@ -255,8 +296,11 @@ git clone https://github.com/htty/htty.git htty
 echo "htty downloaded"
 echo ""
 fi
+fi
 
 # Scrapy from github
+if grep -q "*scrapy" ./.security_toolbox
+then
 if test -d scrapy
 then
 echo "Updating Scrapy from github"
@@ -271,8 +315,11 @@ git clone https://github.com/scrapy/scrapy.git scrapy
 echo "Scrapy downloaded"
 echo ""
 fi
+fi
 
 # Recon-ng from Bitbucket
+if grep -q "*recon-ng" ./.security_toolbox
+then
 if test -d recon-ng
 then
 echo "Updating Recon-ng from bitbucket"
@@ -287,8 +334,11 @@ git clone https://bitbucket.org/LaNMaSteR53/recon-ng.git recon-ng
 echo "Recon-ng downloaded"
 echo ""
 fi
+fi
 
 # zmap from github
+if grep -q "*zmap" ./.security_toolbox
+then
 if test -d zmap
 then
 echo "Updating zmap from github"
@@ -303,8 +353,11 @@ git clone https://github.com/zmap/zmap.git zmap
 echo "zmap downloaded"
 echo ""
 fi
+fi
 
 # zgrab from github
+if grep -q "*zgrab" ./.security_toolbox
+then
 if test -d zgrab
 then
 echo "Updating zgrab from github"
@@ -319,8 +372,11 @@ git clone https://github.com/zmap/zgrab.git zgrab
 echo "zgrab downloaded"
 echo ""
 fi
+fi
 
 # Shodan
+if grep -q "*shodan-python" ./.security_toolbox
+then
 if test -d shodan-python
 then
 echo "Updating Shodan"
@@ -335,8 +391,11 @@ git clone https://github.com/achillean/shodan-python.git shodan-python
 echo "Shodan downloaded"
 echo ""
 fi
+fi
 
 # dnscat2
+if grep -q "*dnscat2" ./.security_toolbox
+then
 if test -d dnscat2
 then
 echo "Updating dnscat2"
@@ -351,8 +410,11 @@ git clone https://github.com/iagox86/dnscat2.git dnscat2
 echo "dnscat2 downloaded"
 echo ""
 fi
+fi
 
 # testssl.sh
+if grep -q "*testssl" ./.security_toolbox
+then
 if test -d testssl
 then
 echo "Updating testssl"
@@ -367,8 +429,11 @@ git clone https://github.com/drwetter/testssl.sh.git testssl
 echo "testssl downloaded"
 echo ""
 fi
+fi
 
 # Gryffin
+if grep -q "*gryffin" ./.security_toolbox
+then
 if test -d gryffin
 then
 echo "Updating Gryffin"
@@ -383,8 +448,11 @@ git clone https://github.com/yahoo/gryffin.git gryffin
 echo "Gryffin downloaded"
 echo ""
 fi
+fi
 
 # Subbrute
+if grep -q "*subbrute" ./.security_toolbox
+then
 if test -d subbrute
 then
 echo "Updating Subbrute"
@@ -399,8 +467,11 @@ git clone https://github.com/TheRook/subbrute.git subbrute
 echo "Subbrute downloaded"
 echo ""
 fi
+fi
 
 # datasploit
+if grep -q "*datasploit" ./.security_toolbox
+then
 if test -d datasploit
 then
 echo "Updating datasploit"
@@ -415,8 +486,11 @@ git clone https://github.com/upgoingstar/datasploit.git datasploit
 echo "datasploit downloaded"
 echo ""
 fi
+fi
 
 # Gophish
+if grep -q "*gophish" ./.security_toolbox
+then
 if test -d gophish
 then
 echo "Updating Gophish"
@@ -431,8 +505,11 @@ git clone https://github.com/gophish/gophish.git gophish
 echo "Gophish downloaded"
 echo ""
 fi
+fi
 
 # Dissembling-Ferret
+if grep -q "*dissembling-ferret" ./.security_toolbox
+then
 if test -d dissembling-ferret
 then
 echo "Updating Dissembling-Ferret"
@@ -446,4 +523,5 @@ echo "Getting Dissembling-Ferret"
 git clone https://github.com/clayball/Dissembling-Ferret.git dissembling-ferret
 echo "Dissembling-Ferret downloaded"
 echo ""
+fi
 fi
