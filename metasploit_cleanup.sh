fc75bf687a666282f334414a8ae9adbe91ceab97
Ben <ben@thegeekzone.com>
2019-01-22 11:27:08 -0600

    Update metasploit_cleanup.sh

--- metasploit_cleanup.sh
+++ metasploit_cleanup.sh
@@ -3,6 +3,6 @@
 # Commands to clean a git repo when a "unable to update local ref" error occurs
 # Has happened to me several times with Metasploit's git repo
 
-cd metasploit_git
+cd metasploit
 git gc --prune=now
 git remote prune origin
