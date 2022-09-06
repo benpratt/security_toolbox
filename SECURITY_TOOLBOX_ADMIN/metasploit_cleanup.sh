cf47d458d6cfb3354af60e8eb4bef16377230d5c
Ben <ben@thegeekzone.com>
2022-09-06 14:13:34 -0500

    Added a couple management scripts

--- SECURITY_TOOLBOX_ADMIN/metasploit_cleanup.sh
+++ SECURITY_TOOLBOX_ADMIN/metasploit_cleanup.sh
@@ -3,6 +3,6 @@
 # Commands to clean a git repo when a "unable to update local ref" error occurs
 # Has happened to me several times with Metasploit's git repo
 
-cd metasploit
+cd ~/security_toolbox/metasploit
 git gc --prune=now
 git remote prune origin
---
+++ SECURITY_TOOLBOX_ADMIN/nmap_configure.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Commands to configure Nmap using a user installed OpenSSL version
+
+cd ~/security_toolbox/nmap
+./configure --with-openssl=/usr/local
