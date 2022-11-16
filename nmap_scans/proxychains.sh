786548b81f94dc4825d8295d1ed2ba3af9508118
Ben <ben@thegeekzone.com>
2022-11-16 16:46:04 -0600

    Added nmap scripts folder and the first script

---
+++ nmap_scans/proxychains.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/bash
+
+# Configure proxychains
+#socks4 127.0.0.1 2222
+
+# Establish ssh proxy tunnel
+#ssh -D 0.0.0.0:2222 ben@server
+
+# Execute scan over tunnel
+#proxychains nmap -Pn -n target
