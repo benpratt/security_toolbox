4dff92f5d8e67345e2789d4a79dae002d49c7b1e
Ben <ben@thegeekzone.com>
2025-05-22 07:24:33 -0500

    Update nmap_configure.sh

--- SECURITY_TOOLBOX_ADMIN/nmap_configure.sh
+++ SECURITY_TOOLBOX_ADMIN/nmap_configure.sh
@@ -3,4 +3,4 @@
 # Commands to configure Nmap using a user installed OpenSSL version
 
 cd ~/security_toolbox/nmap
-./configure --with-openssl=/usr/local --without-zenmap --without-ndiff
+./configure --with-openssl=/usr/local --without-zenmap
